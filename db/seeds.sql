18e93f40e842872b134abce7decbb2323730c3b0
Michael <michaelbao13@gmail>
2019-05-04 03:25:27 -0700

    fin

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Angus Beef Avocado Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cali Chicken Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southern Style BBQ Ribs Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Pepper Jack Goat Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Taco Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ramen Tonkatsu Curry Burger', true);
